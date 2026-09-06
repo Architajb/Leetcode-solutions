865300d70dd91a40d45774e02b54ec83835f00ff
Architajb <179248064+Architajb@users.noreply.github.com>
2026-09-06 17:02:13 +0530

    Time: 8 ms (44.78%), Space: 9.5 MB (76.36%) - LeetHub

---
+++ 4043-count-rotations-with-exactly-k-equal-adjacent-pairs/4043-count-rotations-with-exactly-k-equal-adjacent-pairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countRotations(string s, int k) {
+        int n = s.size();
+        int ans = 0;
+        for (int start = 0; start < n; start++) {
+            int score = 0;
+            for (int i = 0; i < n - 1; i++) {
+                char a = s[(start + i) % n];
+                char b = s[(start + i + 1) % n];
+                if (a == b)
+                    score++;
+            }
+            if (score == k)
+                ans++;
+        }
+        return ans;
+    }
+};
