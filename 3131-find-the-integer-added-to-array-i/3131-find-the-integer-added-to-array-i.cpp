3fa26330d4176da08ad43a7655efdb7d4c38930d
Architajb <179248064+Architajb@users.noreply.github.com>
2026-09-09 23:16:22 +0530

    Time: 0 ms (100%), Space: 35.2 MB (68.03%) - LeetHub

--- 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.cpp
+++ 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     int addedInteger(vector<int>& nums1, vector<int>& nums2) {
-        sort(nums1.begin(),nums1.end());
-        sort(nums2.begin(),nums2.end());
-        return nums2[0]-nums1[0];
+        int min1=INT_MAX;
+        int min2=INT_MAX;
+        for(int num:nums1) min1=min(num,min1);
+        for(int num:nums2) min2=min(num,min2);
+        return min2-min1;
     }
 };
